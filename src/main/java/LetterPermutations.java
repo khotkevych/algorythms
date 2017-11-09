2d634e3671e0832ea09899112bee57fd2b574765
MKateryna Khotkevych <kkhotkevych@gmail.com>
2017-11-09 19:52:51 +0200

    list

--- src/main/java/LetterPermutations.java
+++ src/main/java/LetterPermutations.java
@@ -8,26 +8,25 @@ public class LetterPermutations {
     // combination Ckn = n! / k!(n-k)! - without order
     // permutation Pn = n!
 
-    private static void permutation(String str) {
-        permutation("", str);
+    private static List<String> permutation(String str) {
+        return permutation("", str);
     }
 
-    private static void permutation(String prefix, String str) {
+    private static List<String> permutation(String prefix, String str) {
+        List<String> permutations = new ArrayList<>();
         int n = str.length();
-        System.out.println("prefix=" + prefix + ". str=" + str);
+        System.out.println("prefix=\"" + prefix + "\" str=\"" + str + "\"");
         if (n == 1) {
             System.out.println(prefix + str);
+            permutations.add(prefix + str);
+            return permutations;
         } else {
             for (int i = 0; i < n; i++) {
-                System.out.println("i=" + i + " n=" + n);
-                permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
+                System.out.println("In i=" + i + " n=" + n);
+                permutations.addAll(permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n)));
                 System.out.println("Out i=" + i + " n=" + n);
             }
         }
-    }
-
-    private static List<String> getPermutations(String str) {
-        List<String> permutations = new ArrayList<>();
         return permutations;
     }
 
@@ -94,10 +93,9 @@ public class LetterPermutations {
         System.out.println((endTime - startTime) / 1000000);*/
 
 
-        String str = "abc";
-        //List<String> permutations = getPermutations(str);
-        //System.out.println(factorial(str.length()) + " " + permutations.size());
-        //System.out.println(permutations);
-        permutation(str);
+        String str = "1234";
+        List<String> permutations = permutation(str);
+        System.out.println("\n\n\n" + factorial(str.length()) + " " + permutations.size());
+        System.out.println(permutations);
     }
 }
