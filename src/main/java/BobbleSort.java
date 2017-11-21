748518c3c16ea8f99202db08ddd0ab3f25c05574
MKateryna Khotkevych <kkhotkevych@gmail.com>
2017-11-21 17:55:15 +0200

    quick sort

---
+++ src/main/java/BobbleSort.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class BobbleSort {
+
+    private static void bubblesort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            boolean is_sorted = true;
+
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    is_sorted = false;
+                }
+            }
+            System.out.println(Arrays.toString(a));
+            if (is_sorted) return;
+        }
+    }
+
+    private static void bubblesortBack(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            boolean is_sorted = true;
+
+            for (int j = 0; j < a.length - 1 -i ; j++) {
+                if (a[j] < a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    is_sorted = false;
+                }
+            }
+            System.out.println(Arrays.toString(a));
+            if (is_sorted) return;
+        }
+    }
+
+    private static void bubblesortFast(int[] a) {
+        int lastSwap = a.length - 1;
+        for (int i = 1; i < a.length; i++) {
+            boolean is_sorted = true;
+            int currentSwap = -1;
+
+            for (int j = 0; j < lastSwap; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    is_sorted = false;
+                    currentSwap = j;
+                }
+            }
+            System.out.println(Arrays.toString(a));
+            if (is_sorted) return;
+            lastSwap = currentSwap;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{5, 3, 7, 8, 4, 9, 1};
+        System.out.println(Arrays.toString(a));
+        bubblesortBack(a);
+    }
+}
