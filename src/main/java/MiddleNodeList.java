d95585235c33aae87af994d25be320d5dec12873
MKateryna Khotkevych <kkhotkevych@gmail.com>
2017-11-30 18:35:51 +0200

    fixes

--- src/main/java/MiddleNodeList.java
+++ src/main/java/MiddleNodeList.java
@@ -1,8 +1,9 @@
-import java.util.LinkedList;
+import java.util.Arrays;
 
 public class MiddleNodeList {
 
     private static Node head;
+    private int size;
 
     class Node {
         int data;
@@ -18,11 +19,19 @@ public class MiddleNodeList {
         Node node = new Node(data);
         node.next = head;
         head = node;
+        size++;
+    }
+
+
+    private Object[] toArray() {
+        Object[] result = new Object[size];
+        int i = 0;
+        for (Node x = head; x != null; x = x.next)
+            result[i++] = x.data;
+        return result;
     }
 
     public static void main(String[] args) {
-        LinkedList<String> a = new LinkedList<>();
-        a.add(":");
         MiddleNodeList list = new MiddleNodeList();
         list.add(1);
         list.add(2);
@@ -39,5 +48,64 @@ public class MiddleNodeList {
             fast = fast.next.next;
         }
         System.out.println(slow.data); // 4
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+}
+
+class MiddleNodeList2 {
+
+    private static Node first;
+    private static Node last;
+    private int size;
+
+    class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        Node(int data, Node prev) {
+            this.data = data;
+            this.prev = prev;
+        }
+    }
+
+    private void add(int data) {
+        final Node l = last;
+        Node node = new Node(data, l);
+        last = node;
+        if (first == null)
+            first = last;
+        else
+            //node.prev.next = node;
+            node.next = node;
+        size++;
+    }
+
+    private Object[] toArray() {
+        Object[] result = new Object[size];
+        int i = 0;
+        for (Node x = first; x != null; x = x.next)
+            result[i++] = x.data;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MiddleNodeList2 list = new MiddleNodeList2();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        Node slow = first;
+        Node fast = first;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        System.out.println(slow.data); // 5
+        System.out.println(Arrays.toString(list.toArray()));
     }
 }
--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,9 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class ReverseString {
 
+    // todo deal with punctuation marks
+    private static String swapSentence(String sentence) {
+        String[] str = sentence.split(" ");
+        for (int i=0;i<str.length;i++) {
+            if (str[i].length() > 1) {
+                str[i] = swapWord(str[i]);
+            }
+        }
+        List<String> list = new ArrayList<>(Arrays.asList(str));
+        Collections.reverse(list);
+        return String.join(" ", list);
+    }
+
+    private static String swapWord(String word) {
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < word.length() / 2; i++) {
+            swap(arr, i, word.length() - 1 - i);
+        }
+        return new String(arr);
+    }
+
+    private static void swap(char[] arr, int x, int y) {
+        char tmp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = tmp;
+    }
 
     public static void main(String[] args) {
-        String hello = "HelloWorld123456789";
+        System.out.println(swapSentence("Hello World, 1!"));
+        /*String hello = "HelloWorld123456789";
         char str[] = hello.toCharArray();
         char tmp;
         for (int i = 0; i < str.length - 1; i++) {
@@ -17,10 +48,10 @@ public class ReverseString {
         str = hello.toCharArray();
         for (int i = 0; i < (str.length - 1) / 2 + 1; i++) {
             tmp = str[i];
-            str[i] = str[str.length -1 - i];
-            str[str.length -1 - i] = tmp;
+            str[i] = str[str.length - 1 - i];
+            str[str.length - 1 - i] = tmp;
         }
-        System.out.println(str);
+        System.out.println(str);*/
 
     }
 }
